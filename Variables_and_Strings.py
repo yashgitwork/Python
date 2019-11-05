84e95b51421d0e2759959acbef1d64064f5d9485
yashgitwork <50210398+yashgitwork@users.noreply.github.com>
2019-11-05 10:51:11 +0530

    Create Variables_and_Strings.py

---
+++ Variables_and_Strings.py
@@ -0,0 +1,12 @@
+#Variables are used to store values.
+#String is a series of characters, surrounded by single or double quotes.
+
+#Print hello world.
+
+print "Hello World with double quotes, Not allowed in Pyhton 3, Need round brackets"
+print 'Hello World with single quotes, Not allowed in Pyhton 3, Need round brackets'
+print ("Hello World with round brackets and double quotes")
+print ('Hello World with single quotes and round brackets')
+
+str1= 'Print With a string stored in variable'
+print (str1)
