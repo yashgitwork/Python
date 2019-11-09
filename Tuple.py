d72fa646359daa639f2f6d7fdcb2fc027c214c8a
yashgitwork <50210398+yashgitwork@users.noreply.github.com>
2019-11-09 13:43:52 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+#Tuple is like list but is immutable i.e. it cannot be modified.
+
+tup1=('1','One',"Two",6)
+tup2=("3",)
+tup3='one','two','three'
+tup4=()
+tup5=1,2,3,4,5
+print (tup1)
+print (tup2)
+print (tup3)
+print (tup4)
+print (tup5)
+print (tup3[1])
+print (tup1[1:3])
