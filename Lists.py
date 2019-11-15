5cf1f73fa4a3000cc31a3261ef4bcaafda1099d0
yashgitwork <50210398+yashgitwork@users.noreply.github.com>
2019-11-15 11:00:56 +0530

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -31,3 +31,32 @@ for x in range(1,10):
 
 for x in range(1,9): 
  print(squares[x])
+
+
+#Slicing a List: A portion of a list is called slice.To slice a list start with the index of first item you want, put a colon and the index
+#                of the last item you want.Leave off the first index to start at the beginning of the list, and leave off the last index to
+#                slice throught the end of the list.
+
+
+
+
+
+companies=['Intel', 'Mellanox', 'Google', 'Marvell', 'Qlogic', 'Cavium', 'Xoriant']
+
+#getting the first three elements.
+first_three=companies[:3]
+print (first_three)
+
+#Getting the first to 
+first_to_third=companies[1:4]
+print(first_to_third)
+
+#Getting the last three elements.
+
+last_three=companies[-3:]
+print(last_three)
+
+#Copying a list
+
+copy_of_companies=companies[:]
+print (copy_of_companies)
